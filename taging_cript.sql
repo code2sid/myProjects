9645ff7374ce2038ed5f295e6492de193e705a4a
siddharth gupta <siddharth.gupta@askiitians.com>
2015-10-05 10:55:49 +0530

    delete waste files

--- taging_cript.sql
+++
@@ -1,9 +0,0 @@
-WITH Ques AS (
-SELECT TOP 1000 Description = ' '+CAST(Description AS NVARCHAR(max)) 
-        FROM [Transtutors_QA_7.1].dbo.AssignmentTmp),
-tags AS (SELECT *,crscode =  SUBSTRING(crs,CHARINDEX('-',crs)+1,LEN(crs)) FROM dbo.SchoolDeptsCrs
-WHERE LEN(SUBSTRING(crs,CHARINDEX('-',crs)+1,LEN(crs))) > 3
-)
-
---SELECT * FROM tags t 
-SELECT * FROM tags t INNER JOIN Ques q ON q.Description LIKE '% '+ t.crscode +' %'
