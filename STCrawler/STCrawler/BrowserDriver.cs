de48209d240cd00f79cc83568fd4b60fd51a0bb1
Gupta <SIDDHARTH.GUPTA@int.asurion.com>
2016-09-12 15:51:35 +0530

    bins and chromdriver

--- STCrawler/STCrawler/BrowserDriver.cs
+++ STCrawler/STCrawler/BrowserDriver.cs
@@ -23,13 +23,15 @@ namespace STCrawler
         [SetUp]
         public void setup()
         {
-            
+
             Console.WriteLine("Which driver (c/m/i)");
             var opt = Console.ReadLine();
             if (opt.ToLower().Contains("m"))
                 driver = new FirefoxDriver();
             else if (opt.ToLower().Contains("c"))
                 driver = new ChromeDriver(@"C:\Users\siddharth.gupta\AppData\Local\Google\Chrome SxS\Application");
+            else
+                driver = new FirefoxDriver();
         }
 
         public void testConnection()
@@ -45,7 +47,7 @@ namespace STCrawler
                 Console.ReadLine();
                 Environment.Exit(0);
             }
-            
+
         }
 
         [Test]
@@ -63,7 +65,7 @@ namespace STCrawler
 
             Console.WriteLine("Enter username: ");
             var username = Console.ReadLine();
-            var password ="smile1510";
+            var password = "smile1510";
             if (string.Compare(username, "ruchi") == 0)
                 username = "61053682";
             else
@@ -71,7 +73,7 @@ namespace STCrawler
                 Console.WriteLine("Enter password: ");
                 password = Utilitiy.ReadLineMasked();
             }
-            
+
             driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$txtEmailID")).SendKeys(username);
             driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$txtPassword")).SendKeys(password);
             driver.FindElement(By.Name("ctl00$ContentPlaceHolder1$CndSignIn")).Click();
@@ -105,39 +107,42 @@ namespace STCrawler
 
             }
 
-        }
+        }//*[@id="ctl00_ContentPlaceHolder1_gvAssignment_ctl03_Panel4"]/a
+        //*[@id="ctl00_ContentPlaceHolder1_gvAssignment_ctl10_Panel4"]
 
-        public void ExecuteClicks(int strt =1 , int stp = 250)
+        public void ExecuteClicks(int strt = 1, int stp = 250)
         {
+            var linkNo = "0";
+            var tabCnt = 0;
+            IList<string> allWindowHandles = null;
+            
             for (int i = strt; i <= stp; i++)
             {
+                linkNo = (i + 1).ToString().Length < 2 ? string.Concat("0", i + 1) : (i + 1).ToString();
                 Console.WriteLine(string.Format("clicking row:{0} link", i));
                 try
                 {
-                    if (i <= 20)
+                    allWindowHandles = driver.WindowHandles;
+                    foreach (var item in allWindowHandles)
+                    {
+                        if (item == driver.CurrentWindowHandle)
+                            tabCnt++;
+                    }
+                    if (tabCnt <= 20)
                     {
                         driver.FindElement(By.TagName("body")).SendKeys(Keys.Control + "t");
                         driver.Navigate().GoToUrl(todayTaskPath);
 
-                        if (driver.FindElements(By.XPath(".//*[@id='ctl00_ContentPlaceHolder1_UpPanel1']/div/table/tbody/tr["
-                            + (i + 1) + "]/td[4]/div/table/tbody/tr/td[1]/div/a")).Count > 0)
-                        {
-                            driver.FindElement(By.XPath(".//*[@id='ctl00_ContentPlaceHolder1_UpPanel1']/div/table/tbody/tr["
-                                    + (i + 1) + "]/td[4]/div/table/tbody/tr/td[1]/div/a")).Click();
-                        }
+                        if (driver.FindElements(By.XPath(string.Format("//*[@id='ctl00_ContentPlaceHolder1_gvAssignment_ctl{0}_Panel4']/a", linkNo))).Count > 0)
+                            driver.FindElement(By.XPath(string.Format("//*[@id='ctl00_ContentPlaceHolder1_gvAssignment_ctl{0}_Panel4']/a", linkNo))).Click();
                     }
 
                     else
                     {
                         driver.FindElement(By.TagName("body")).SendKeys(Keys.Control + "\t");
                         driver.Navigate().Refresh();
-                        if (driver.FindElements(By.XPath(".//*[@id='ctl00_ContentPlaceHolder1_UpPanel1']/div/table/tbody/tr["
-                            + (i + 1) + "]/td[4]/div/table/tbody/tr/td[1]/div/a")).Count > 0)
-                        {
-                            driver.FindElement(By.XPath(".//*[@id='ctl00_ContentPlaceHolder1_UpPanel1']/div/table/tbody/tr["
-                                    + (i + 1) + "]/td[4]/div/table/tbody/tr/td[1]/div/a")).Click();
-
-                        }
+                        if (driver.FindElements(By.XPath(string.Format("//*[@id='ctl00_ContentPlaceHolder1_gvAssignment_ctl{0}_Panel4']/a", linkNo))).Count > 0)
+                            driver.FindElement(By.XPath(string.Format("//*[@id='ctl00_ContentPlaceHolder1_gvAssignment_ctl{0}_Panel4']/a", linkNo))).Click();
                     }
                 }
                 catch (Exception e)
