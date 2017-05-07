1b1f6bd6566c3ae717cb8e3d83b4609184c14dc6
code2sid <write2siddharthg@gmail.com>
2017-05-08 00:35:38 +0530

    iWords

---
+++ iWords/iWords/RevisionCards.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace iWords
+{
+    public partial class RevisionCards : Form
+    {
+        public DataSet dsWordRepos = new DataSet();
+        public RevisionCards()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadWords(int preWord = 0)
+        {
+            DataRow dr = null;
+            dsWordRepos.ReadXml(ConfigurationSettings.AppSettings["configurationfilePath"].ToString());
+            int maxCnt = dsWordRepos.Tables[0].Rows.Count - 1;
+            Random rnd = new Random();
+            var rnumber = preWord == 0 ? rnd.Next(0, maxCnt) : preWord;
+            hdnPreWord.Text = rnumber.ToString();
+            int knwCnts = dsWordRepos.Tables[0].Select("KnowThis=1").ToList().Count;
+            int dntKnwCnts = dsWordRepos.Tables[0].Select("KnowThis=0").ToList().Count;
+            dr = dsWordRepos.Tables[0].Rows[rnumber];
+            if (dr != null)
+                btnWord.Text = dr["Title"].ToString();
+
+        }
+        private void RevisionCards_Load(object sender, EventArgs e)
+        {
+            LoadWords();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            LoadWords();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            LoadWords(int.Parse(hdnPreWord.Text));
+
+        }
+
+        private void btnWord_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WordDetails wd = new WordDetails();
+            foreach (var item in wd.Controls)
+            {
+                if (item.GetType().Equals(typeof(Label)))
+                    if (((Label)item).Name.Equals("lblWord"))
+                        ((Label)item).Text = btnWord.Text;
+            }
+            wd.Show();
+        }
+
+        private void lnkHome_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            SelectOption so = new SelectOption();
+            so.Show();
+        }
+
+        private void btnWord_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnNext_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnPrevious_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void RevisionCards_MouseDown(object sender, MouseEventArgs e)
+        {
+            WordDetails.mouseDown = true;
+            WordDetails.lastLocation = e.Location;
+        }
+
+        private void RevisionCards_MouseUp(object sender, MouseEventArgs e)
+        {
+            WordDetails.mouseDown = false;
+        }
+
+        private void RevisionCards_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (WordDetails.mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - WordDetails.lastLocation.X) + e.X, (this.Location.Y - WordDetails.lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+    }
+}
---
+++ iWords/iWords/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iWords
+{
+    public class Word
+    {
+        public string Title { get; set; }
+        public string Meaning { get; set; }
+        public string Example { get; set; }
+        public int Knowthis { get; set; }
+        public int knowCnt { get; set; }
+        public int dontKnwCnt { get; set; }
+    }
+}
